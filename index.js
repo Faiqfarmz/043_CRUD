1a7e61b05c2e30a7f47872c02da930d791e00773
Faiqfarmz <faiqfarmz@gmail.com>
2026-07-15 15:13:04 +0700

    post

--- index.js
+++ index.js
@@ -38,3 +38,20 @@ app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
 });
 
+//post
+app.post('/biodata', async (req, res) => {
+    try {
+        const { id, nama, nim, kelas } = req.body;
+        const result = await pool.query(
+            'INSERT INTO biodata (id, nama, nim, kelas) VALUES ($1, $2, $3, $4) RETURNING *',
+            [id, nama, nim, kelas]
+        );
+        res.status(201).json({
+            message: "Berhasil menambahkan data biodata",
+            data: result.rows[0]
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Terjadi kesalahan pada server atau database" });
+    }
+});
